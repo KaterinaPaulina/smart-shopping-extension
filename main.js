a94646729fd141f08456368860361cf314a21e61
Katerina Paulinova <paulinovakaterina@gmail.com>
2020-02-01 22:56:24 +0100

    button added

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
-function openSettings() {
-   document.getElementById("settings").classList.toggle("settings-open");
+const SmartShoppingExtension = {
+
+  showProductTitle: function () {
+
+      var products = document.querySelectorAll(".productCard__title");
+
+      products.forEach(element => console.log(element.textContent));
+  },
+
+  addButtons: function () {
+    var pricesInfo = document.querySelectorAll(".priceWrap");
+
+    pricesInfo.forEach((price) => {
+        var button = document.createElement("button");
+        button.innerText = 'Compare the price';
+        button.className = 'compare-price-btn';
+        price.appendChild(button);
+
+        // tady bude event on click
+      });
+
+
+  }
 }
 
-document.getElementById("settings-button").addEventListener('click', openSettings)
+
+//window.setInterval(SmartShoppingExtension.addButtons, 1000);
